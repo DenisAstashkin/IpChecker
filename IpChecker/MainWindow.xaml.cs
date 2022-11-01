730fd3dec4efc5a81fb8cf8f17608112876387b7
DenisAstashkin <ast.den27@gmail.com>
2022-11-01 18:26:01 +0400

    Create project

---
+++ IpChecker/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Newtonsoft.Json;
+using RestSharp;
+using System.IO;
+
+
+namespace IpChecker
+{    
+    public partial class MainWindow : Window
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient($"http://ip-api.com/json/{IP.Text}?lang=ru");
+            var request = new RestRequest
+            {
+                Method = Method.Get
+            };
+            var response = await client.ExecuteGetAsync(request);
+            var dic = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+            Info.Children.Clear();
+            for (int i = 0; i < NameInfo.Children.Count; i++)
+            {
+                var ChildLable = NameInfo.Children[i] as Label;
+                var key = new StringBuilder(Convert.ToString(ChildLable.Content));
+                key = key.Replace(": ", string.Empty);
+                key[0] = char.ToLower(key[0]);
+                try
+                {
+                    Info.Children.Add(new Label()
+                    {
+                        Content = dic[key.ToString()],
+                        Foreground = new SolidColorBrush(Colors.Red),
+                        FontWeight = FontWeights.Bold,
+                        FontSize = 17
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Info.Children.Add(new Label()
+                    {
+                        Content = string.Empty
+                    });
+                    continue;
+                }
+            }
+            if (File.Exists($"{AppDomain.CurrentDomain.BaseDirectory}SQLite.Interop.dll"))
+            {
+                var map = new Map();
+                map.lat = Convert.ToDouble(((Info.Children[8] as Label)?.Content as string)?.Replace('.', ','));
+                map.lon = Convert.ToDouble(((Info.Children[9] as Label)?.Content as string)?.Replace('.', ','));
+                map.Show();
+                return;
+            }
+            MessageBox.Show($"Чтобы узнать геолокацию установите SQLite.Interop.dll в папку {AppDomain.CurrentDomain.BaseDirectory}");
+        }
+    }
+}
---
+++ IpChecker/Map.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using GMap.NET;
+using GMap.NET.MapProviders;
+
+namespace IpChecker
+{    
+    public partial class Map : Window
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public Map()
+        {
+            InitializeComponent();
+        }
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            mapView.MapProvider = GoogleMapProvider.Instance;
+            mapView.MinZoom = 2;
+            mapView.MaxZoom = 20;
+            mapView.Zoom = 13; 
+            mapView.Position = new PointLatLng(lat, lon);
+            mapView.MouseWheelZoomType = MouseWheelZoomType.ViewCenter; 
+            mapView.CanDragMap = true;             
+            mapView.ShowCenter = true;
+        }
+    }
+}
